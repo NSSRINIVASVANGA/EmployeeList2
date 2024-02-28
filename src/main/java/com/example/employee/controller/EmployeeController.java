83d6687c7ff0a1aaa2031502a89dd4814fc08884
NSSRINIVASVANGA <vanganataraj787@gmail.com>
2024-02-28 05:53:50 +0000

    solution

---
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -0,0 +1,49 @@
+package com.example.employee.controller;
+
+import com.example.employee.model.*;
+import com.example.employee.repository.*;
+import com.example.employee.service.EmployeeJpaService;
+
+import java.util.*;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    public EmployeeJpaService employeeService;
+
+    @GetMapping("/employees")
+    public ArrayList<Employee> getEmployees() {
+        return employeeService.getEmployees();
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeService.addEmployee(employee);
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable("employeeId") int employeeId) {
+        return employeeService.getEmployeeById(employeeId);
+    }
+
+    @PutMapping("/employees/{employeeId}")
+    public Employee updateEmployee(@PathVariable("employeeId") int employeeId, @RequestBody Employee employee) {
+        return employeeService.updateEmployee(employeeId, employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void deleteEmployee(@PathVariable("employeeId") int employeeId) {
+        employeeService.deleteEmployee(employeeId);
+    }
+}
+/*
+ * You can use the following import statements
+ *
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.*;
+ */
+
+// Write your code here
