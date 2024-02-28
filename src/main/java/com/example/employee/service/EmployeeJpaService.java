1536948a9877bdaca7db7e64316ad1e8a9a5bcc8
Vanga Nataraja Shanmukha Srinivas <156287911+NSSRINIVASVANGA@users.noreply.github.com>
2024-02-28 11:33:25 +0530

    Update EmployeeJpaService.java

--- src/main/java/com/example/employee/service/EmployeeJpaService.java
+++ src/main/java/com/example/employee/service/EmployeeJpaService.java
@@ -65,14 +65,4 @@ public class EmployeeJpaService implements EmployeeRepository {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
     }
-
-/*
- * You can use the following import statements
- * import org.springframework.beans.factory.annotation.Autowired;
- * import org.springframework.http.HttpStatus;
- * import org.springframework.stereotype.Service;
- * import org.springframework.web.server.ResponseStatusException;
- * import java.util.*;
- */
-
-// Write your code here
+}
